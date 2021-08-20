5a271ce8e5296e4a9f0e58f84b716341f2d581c9
Podesta <11512008+Podesta@users.noreply.github.com>
2021-08-20 17:49:53 +0000

    Rendering a collection, modules

---
+++ part-02/course-info/src/components/Total.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce(((initialValue, part) => 
+    initialValue + part.exercises), 0);
+
+  return(
+    <p><b>total of {sum} exercises</b></p>
+  );
+}
+
+export default Total;
