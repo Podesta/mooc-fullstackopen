4d6489cf32f09819d3bf045d41b8d90070a9d713
Podesta <11512008+Podesta@users.noreply.github.com>
2021-08-27 12:41:20 +0000

    data for countries pt2

--- part-02/data-for-countries/src/App.jsx
+++ part-02/data-for-countries/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const SearchBar = ({ country, handleCountry }) => {
   return (
@@ -8,14 +9,59 @@ const SearchBar = ({ country, handleCountry }) => {
   );
 }
 
+const DisplayCountry = ({ countries }) => {
+  if (countries.length > 10) {
+    return (
+      <div>
+        Too many matches, specify another filter.
+      </div>
+    );
+  } else if (countries.length > 1) {
+    return (
+      <div>
+        {countries.map(country =>
+        <div key={country.alpha3Code}>
+          {country.name}
+        </div>
+        )}
+      </div>
+    );
+  } else if (countries.length === 1) {
+    return (
+      <div>
+        <h3>{countries[0].name}</h3>
+        <p>capital: {countries[0].capital}<br/>
+           population: {countries[0].population}
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
 const App = () => {
   const [country, setCountry] = useState('');
+  const [found, setFound] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all').then(response => {
+      const match = response.data.filter(singleCountry => singleCountry.name.toLowerCase().includes(country.toLowerCase()));
+      setFound(match);
+    });
+  }, [country]);
 
   const handleCountry = (event) =>
     setCountry(event.target.value);
 
   return (
-    <SearchBar handleCountry={handleCountry} country={country} />
+    <div>
+      <SearchBar handleCountry={handleCountry} country={country} />
+      <DisplayCountry countries={found} />
+    </div>
   );
 }
 
